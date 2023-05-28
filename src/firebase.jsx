83e2a1d130b605b9403806b4e8ce317a35fa23b6
Sumi-Dumi <77911196+Sumi-Dumi@users.noreply.github.com>
2023-05-28 21:47:32 +1000

    Update code

---
+++ src/firebase.jsx
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBKSPP15lebqNGDKzmN07e56jfL5rqOZFw",
+  authDomain: "twitterclone-ca8eb.firebaseapp.com",
+  projectId: "twitterclone-ca8eb",
+  storageBucket: "twitterclone-ca8eb.appspot.com",
+  messagingSenderId: "601603684511",
+  appId: "1:601603684511:web:714165c69447b6ea4f8aaa"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import firebase from "./firebase"
+
+console.log(firebase);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
