d1aa301818e968730f9f3c2030427df68e2d937b
Sumi-Dumi <77911196+Sumi-Dumi@users.noreply.github.com>
2023-05-28 21:49:58 +1000

    Update code

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import firebase from "./firebase"
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
 
 console.log(firebase);
 
